79ac5f49067a2dbeaaf0a6660a62be7164737eea
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2023-09-19 18:33:18 +0200

    ajout du publicpath

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,8 @@ module.exports = defineConfig({
 })
 //ajout de lignes pour pouvoir publier sur git
 //trouvées sur https://dfaveris.medium.com/publier-une-application-vuejs-sur-github-ee7662c94667
-const publicPath = process.env.NODE_ENV === 'production' ? '/TestVue/' : '/'
+
+const publicPath = process.env.NODE_ENV === 'production' ? '/TestVue-14h/' : '/'
 module.exports = {
   publicPath: publicPath,
 }
